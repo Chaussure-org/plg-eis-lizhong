b18194299916f50feb0848e9e8e7fb90b010f5a8
dengjian@prolog-int.com <dengjian@prolog-int.com>
2020-10-13 18:32:56 +0800

    解决冲突，代码合并

--- prolog-eis-zjlz-master/src/main/java/com/prolog/eis/store/service/IContainerStoreService.java
+++ prolog-eis-zjlz-master/src/main/java/com/prolog/eis/store/service/IContainerStoreService.java
@@ -1,6 +1,7 @@
 package com.prolog.eis.store.service;
 
 import com.prolog.eis.model.ContainerStore;
+import com.prolog.eis.model.GoodsInfo;
 
 /**
  * @author dengj
@@ -14,5 +15,40 @@ public interface IContainerStoreService {
      */
     void updateContainerStore(ContainerStore containerStore);
 
+    /**
+     * 修改库存
+     * @param num
+     * @param containerNo
+     * @throws Exception
+     */
     void updateContainerStoreNum(int num,String containerNo) throws Exception;
+
+
+    /**
+     * 根据托盘获取商品库存信息
+     * @param containerNo
+     * @return
+     */
+    GoodsInfo findContainerStockInfo(String containerNo);
+
+    /**
+     * 获取商品的分配货位属性1
+     * @param goodsInfo
+     * @return
+     */
+    String buildTaskProperty1(GoodsInfo goodsInfo);
+
+    /**
+     * 获取商品的分配货位属性2
+     * @param goodsInfo
+     * @return
+     */
+    String buildTaskProperty2(GoodsInfo goodsInfo);
+
+    /**
+     * 将托盘库存设置位空托盘
+     * @param containerNo 托盘号
+     * @return
+     */
+    boolean setContainerStoreEmpty(String containerNo);
 }
