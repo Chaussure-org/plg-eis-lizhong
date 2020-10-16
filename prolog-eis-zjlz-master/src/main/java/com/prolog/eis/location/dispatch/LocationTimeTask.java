7ff6fd5ecdedfcb0c933819d4c94d2baeed67d7a
wangkang <wangk@prolog-int.com>
2020-10-16 17:06:12 +0800

    修改pom文件及路径相关

---
+++ prolog-eis-zjlz-master/src/main/java/com/prolog/eis/location/dispatch/LocationTimeTask.java
@@ -0,0 +1,34 @@
+package com.prolog.eis.location.dispatch;
+
+import com.prolog.eis.location.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: wuxl
+ * @create: 2020-09-23 17:24
+ * @Version: V1.0
+ */
+@Component
+@EnableAsync
+public class LocationTimeTask {
+
+    @Autowired
+    private LocationService locationService;
+
+
+    /**
+     * 生成并执行路径任务
+     * @throws Exception
+     */
+    @Scheduled(initialDelay = 10000, fixedDelay = 15000)
+    @Async
+    public void doContainerPathTask() throws Exception {
+        locationService.doContainerPathTaskAndExecutionByContainer(null, null);
+    }
+
+}
+
--- prolog-eis-zjlz-master/src/main/java/com/prolog/eis/location/service/PathExecutionService.java
+++ prolog-eis-zjlz-master/src/main/java/com/prolog/eis/location/service/PathExecutionService.java
@@ -6,107 +6,75 @@ import com.prolog.eis.model.location.ContainerPathTask;
 public interface PathExecutionService {
 
     /**
-     * 执行rcs-rcs路径任务
-     * @param containerPathTask
-     * @param containerPathTaskDetailDTO
+     * 执行rcs-rcs路径任务(agv区)
+     * @param containerPathTask 路径任务
+     * @param containerPathTaskDetailDTO 详情实体
      * @throws Exception
      */
     void doRcsToRcsTask(ContainerPathTask containerPathTask, ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
 
     /**
-     * 执行mcs-rcs路径任务
-     * @param containerPathTask
-     * @param containerPathTaskDetailDTO
-     * @throws Exception
+     * 执行sas-sas路径任务
+     * @param containerPathTask 路径任务
+     * @param containerPathTaskDetailDTO 详情实体
      */
-    void doMcsToRcsTask(ContainerPathTask containerPathTask, ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
-
-    /**
-     * 执行rcs-mcs路径任务
-     * @param containerPathTask
-     * @param containerPathTaskDetailDTO
-     * @throws Exception
-     */
-    void doRcsToMcsTask(ContainerPathTask containerPathTask, ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
+    void doSasToSasTask(ContainerPathTask containerPathTask, ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
     
     /**
-     * 执行mcs-mcs路径任务
-     * @param containerPathTask
-     * @param containerPathTaskDetailDTO
+     * 执行mcs-mcs路径任务(堆垛机区)
+     * @param containerPathTask 路径任务
+     * @param containerPathTaskDetailDTO 详情实体
      * @throws Exception
      */
     void doMcsToMcsTask(ContainerPathTask containerPathTask, ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
 
-    ///**
-    // * 执行mcs-wcs路径任务
-    // * @param containerPathTask
-    // * @param containerPathTaskDetailDTO
-    // * @throws Exception
-    // */
-    //void doMcsToWcsTask(ContainerPathTask containerPathTask,ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
-    //
-    ///**
-    // * 执行wcs-mcs路径任务
-    // * @param containerPathTask
-    // * @param containerPathTaskDetailDTO
-    // * @throws Exception
-    // */
-    //void doWcsToMcsTask(ContainerPathTask containerPathTask,ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
-    //
-    ///**
-    // * 执行wcs-mcs路径任务
-    // * @param containerPathTask
-    // * @param containerPathTaskDetailDTO
-    // * @throws Exception
-    // */
-    //void doWcsToRcsTask(ContainerPathTask containerPathTask,ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
-    //
-    ///**
-    // * 执行rcs-wcs路径任务
-    // * @param containerPathTask
-    // * @param containerPathTaskDetailDTO
-    // * @throws Exception
-    // */
-    //void doRcsToWcsTask(ContainerPathTask containerPathTask,ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
-    //
-    ///**
-    // * 执行sas-wcs路径任务
-    // * @param containerPathTask
-    // * @param containerPathTaskDetailDTO
-    // * @throws Exception
-    // */
-    //void doSasToWcsTask(ContainerPathTask containerPathTask,ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
-    //
-    ///**
-    // * 执行wcs-sas路径任务
-    // * @param containerPathTask
-    // * @param containerPathTaskDetailDTO
-    // * @throws Exception
-    // */
-    //void doWcsToSasTask(ContainerPathTask containerPathTask,ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
-
-
-
+    /**
+     * 执行mcs-wcs路径任务(堆垛机到输送线)
+     * @param containerPathTask 路径任务
+     * @param containerPathTaskDetailDTO 详情实体
+     * @throws Exception
+     */
+    void doMcsToWcsTask(ContainerPathTask containerPathTask,ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
 
     /**
-     * 执行sas-sas路径任务
-     * @param containerPathTask
-     * @param containerPathTaskDetailDTO
+     * 执行wcs-mcs路径任务(输送线到堆垛机)
+     * @param containerPathTask 路径任务
+     * @param containerPathTaskDetailDTO 详情实体
+     * @throws Exception
      */
-    void doSasToSasTask(ContainerPathTask containerPathTask, ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
+    void doWcsToMcsTask(ContainerPathTask containerPathTask,ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
 
     /**
-     * sas-mcs路径任务
-     * @param containerPathTask
-     * @param containerPathTaskDetailDTO
+     * 执行wcs-rcs路径任务(输送线到agv)
+     * @param containerPathTask 路径任务
+     * @param containerPathTaskDetailDTO 详情实体
+     * @throws Exception
      */
-    void doSasToMcsTask(ContainerPathTask containerPathTask, ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
+    void doWcsToRcsTask(ContainerPathTask containerPathTask,ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
 
     /**
-     * 执行mcs-sas路径任务
-     * @param containerPathTask
-     * @param containerPathTaskDetailDTO
+     * 执行rcs-wcs路径任务(agv到输送线)
+     * @param containerPathTask 路径任务
+     * @param containerPathTaskDetailDTO 详情实体
+     * @throws Exception
      */
-    void doMcsToSasTask(ContainerPathTask containerPathTask, ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
+    void doRcsToWcsTask(ContainerPathTask containerPathTask,ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
+
+    /**
+     * 执行sas-wcs路径任务(提升机到输送线)
+     * @param containerPathTask 路径任务
+     * @param containerPathTaskDetailDTO 详情实体
+     * @throws Exception
+     */
+    void doSasToWcsTask(ContainerPathTask containerPathTask,ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
+
+    /**
+     * 执行wcs-sas路径任务(输送线到提升机)
+     * @param containerPathTask 路径任务
+     * @param containerPathTaskDetailDTO 详情实体
+     * @throws Exception
+     */
+    void doWcsToSasTask(ContainerPathTask containerPathTask,ContainerPathTaskDetailDTO containerPathTaskDetailDTO) throws Exception;
+
 
 }
