0fa54066abf8f5c0b7f1c05272e8aa1dc734fed1
sunpp <sunpengpeng@prolog-int.com>
2020-10-26 18:04:07 +0800

    小车跨层

--- prolog-eis-zjlz-master/src/main/java/com/prolog/eis/out/service/EmptyBoxBackServiceImpl.java
+++ prolog-eis-zjlz-master/src/main/java/com/prolog/eis/out/service/EmptyBoxBackServiceImpl.java
@@ -6,12 +6,13 @@ import com.prolog.eis.dto.out.EmptyBoxBackDto;
 import com.prolog.eis.dto.wcs.CarInfoDTO;
 import com.prolog.eis.engin.service.BoxOutEnginService;
 import com.prolog.eis.location.service.PathSchedulingService;
+import com.prolog.eis.model.ContainerStore;
 import com.prolog.eis.model.location.StoreArea;
 import com.prolog.eis.sas.service.ISASService;
 import com.prolog.eis.store.dao.ContainerStoreMapper;
 import com.prolog.eis.wcs.service.IWCSService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.security.sasl.SaslServer;
 import java.util.Arrays;
@@ -26,7 +27,6 @@ import java.util.stream.Collectors;
  * @return
  * @date:2020/10/26 10:49
  */
-@Service
 public class EmptyBoxBackServiceImpl implements EmptyBoxBackService {
 
     @Autowired
@@ -34,17 +34,23 @@ public class EmptyBoxBackServiceImpl implements EmptyBoxBackService {
 
     @Autowired
     private BoxOutEnginService boxOutEnginService;
+    @Autowired
+    private ContainerStoreMapper containerStoreMapper;
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void outEmptyBox(EmptyBoxBackDto emptyBoxBackDto) throws Exception {
 
         List<OutContainerDto> emptyBoxs = boxOutEnginService.outByGoodsId(-10, emptyBoxBackDto.getQTY());
-        if (emptyBoxs.size()<emptyBoxBackDto.getQTY()){
-            throw new Exception("当前空料箱数量"+emptyBoxs.size()+"不满足出库数量"+emptyBoxBackDto.getQTY());
+        if (emptyBoxs.size() < emptyBoxBackDto.getQTY()) {
+            throw new Exception("当前空料箱数量" + emptyBoxs.size() + "不满足出库数量" + emptyBoxBackDto.getQTY());
         }
-        for (int i=0;i<emptyBoxBackDto.getQTY();i++){
+        for (int i = 0; i < emptyBoxBackDto.getQTY(); i++) {
             pathSchedulingService.containerMoveTask(emptyBoxs.get(i).getContainerNo(),
-                    StoreArea.WCS061,null);
+                    StoreArea.WCS061, null);
         }
+        List<String> containers = emptyBoxs.stream().map(OutContainerDto::getContainerNo).collect(Collectors.toList());
+        String strs = String.join(",", containers);
+        containerStoreMapper.updateContainerStatus(strs, ContainerStore.TASK_TYPE_INVENTORY_BACK);
     }
 }
