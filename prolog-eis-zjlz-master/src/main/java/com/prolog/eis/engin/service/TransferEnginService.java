1300ebbb5096631b3dd732569e6c9e941831f6bf
sunpp <sunpengpeng@prolog-int.com>
2020-10-27 11:09:11 +0800

    小车跨层

---
+++ prolog-eis-zjlz-master/src/main/java/com/prolog/eis/engin/service/TransferEnginService.java
@@ -0,0 +1,11 @@
+package com.prolog.eis.engin.service;
+
+/**
+ * @Author SunPP
+ * myMotto:三十功名尘与土，八千里路云和月
+ * Description:移库作业调度
+ * @return
+ * @date:2020/10/27 11:07
+ */
+public interface TransferEnginService {
+}
---
+++ prolog-eis-zjlz-master/src/main/java/com/prolog/eis/engin/service/impl/TransferEnginServiceImpl.java
@@ -0,0 +1,15 @@
+package com.prolog.eis.engin.service.impl;
+
+import com.prolog.eis.engin.service.TransferEnginService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author SunPP
+ * myMotto:三十功名尘与土，八千里路云和月
+ * Description:移库作业调度实现
+ * @return
+ * @date:2020/10/27 11:08
+ */
+@Service
+public class TransferEnginServiceImpl implements TransferEnginService {
+}
